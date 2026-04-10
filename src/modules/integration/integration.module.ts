ebdf71d2ba310e731ce97411c9998f4e234d018a
Arun-Nithya06 <arun@amwhiz.com>
2026-04-10 19:03:50 +0530

    remove: test route

--- src/modules/integration/integration.module.ts
+++ src/modules/integration/integration.module.ts
@@ -4,11 +4,10 @@ import { DatabaseModule } from '@common/database/database.module';
 import { IntegrationService } from './integration.service';
 import { OdooModule } from '@modules/odoo/odoo.module';
 import { HubspotModule } from '@modules/hubspot/hubspot.module';
-import { HubspotController } from './intergration.controller';
 
 @Module({
   imports: [DatabaseModule, OdooModule, HubspotModule],
-  controllers: [HubspotController],
+  controllers: [],
   providers: [IntegrationService],
   exports: [IntegrationService],
 })
