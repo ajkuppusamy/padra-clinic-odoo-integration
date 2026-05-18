06c6703811d4671110906a63c74d0e88eff2d6ef
ajkuppusamy <Akuppusamy@mypadra.com>
2026-05-18 12:34:56 +0530

    fix: odoo unwanted properties removed

--- src/modules/odoo/odoo.service.ts
+++ src/modules/odoo/odoo.service.ts
@@ -500,7 +500,7 @@ export class OdooService {
         ? {
             sale_service_type_id,
             sale_treatment_category_id,
-            smr_amount_discount: Number(properties?.properties?.discount_amount ?? 0),
+            // smr_amount_discount: Number(properties?.properties?.discount_amount ?? 0), // this property is currently not mapped in odoo, need to create custom field and map it in odoo before using this
             no_of_hairs: Number(properties?.properties?.number_of_hairs ?? properties?.properties?.number_of_hairs___cloned_ ?? 0),
             session: Number(properties?.properties?.number_of_sessions ?? 0),
             sessions_completed: Number(properties?.properties?.sessions_completed ?? 0),
